1791e16fa7f66684790d18acf3ce15199823d784
Justus Storm <storm.j.dev@gmail.com>
2020-08-24 13:35:30 -0500

    Completed the exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,7 +6,24 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // explicit typing
+            string variableName = "Hello";
+
+            // inferred typing
+            var varName = "Hello again";
+            
+            
+            var a = 5;
+            var b = 10;
+            var div = a / b;
+            var mod = a % b;
+            Console.WriteLine($"{a} / {b} is {div} with a remainder of {mod}.");
+
+            var r = 20;
+            var pi = Math.PI;
+            var areaOfCircle = pi * (r * r);
+            Console.WriteLine($"{r} is the radius that was given, and using that info we calculate the area is {areaOfCircle}");
+
         }
     }
 }
